f2bb80ceae5c1172d933a7135f91734721769a74
B-Deforce <bojedeforce@gmail.com>
2021-04-14 15:40:28 +0200

    added XGB

--- assignment1/cleanup_notebook.sh
+++ assignment1/cleanup_notebook.sh
@@ -2,4 +2,4 @@
 
 # Remove the output of a notebook, so that we can store it clean in git.
 # To be executed before 'git add'
-python3 -m nbconvert --clear-output *.ipynb **/*.ipynb
+python -m nbconvert --clear-output *.ipynb **/*.ipynb
