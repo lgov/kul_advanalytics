39380092a1f65c49d108a8b19b262f1676406efb
Lieven Govaerts <lgo@mobsol.be>
2021-04-30 14:41:00 +0200

    Add an AutoGluon based script which finds the best model automatically in 20 minutes. Current results: 473K/6th place.

---
+++ assignment1/auto_gluon.py
@@ -0,0 +1,25 @@
+from autogluon.tabular import TabularDataset, TabularPredictor
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('train.csv', sep=";", encoding="utf-8-sig")
+df = df.drop(columns=["claim_amount"])
+# claim_amount is currently dropped since poor performance
+train_df,val_df = train_test_split(df, test_size=0.2,  random_state=96)
+
+train_data = TabularDataset(train_df)
+val_data = TabularDataset(val_df)
+predictor = TabularPredictor(label='fraud').fit(train_data, time_limit=1200,
+                                                num_bag_folds=5, num_bag_sets=1, num_stack_levels=1)  # Fit models for 120s
+leaderboard = predictor.leaderboard(val_data)
+print(leaderboard)
+
+submission = pd.read_csv('test.csv', sep=";", encoding="utf-8-sig")
+submission["prediction"] = predictor.predict_proba(submission)['Y']
+submit_pred = submission.copy()
+# final submission set initialization
+submission = submission.reset_index()[['claim_id']]
+submission["prediction"] = submit_pred['prediction']
+submission.columns = ["ID", "PROB"]
+print(submission)
+submission.to_csv("submission_V0.71.csv", sep=',', index=False)
